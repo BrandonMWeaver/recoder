700792a936cb57f6987d18e302313eec66319c52
Brandon Weaver <a7xncob@gmail.com>
2020-01-21 15:31:23 -0500

    small refactor/moved session actions to sessions_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,42 +15,6 @@ class ApplicationController < Sinatra::Base
 		erb :index
 	end
 
-	get "/register" do
-		erb :register
-	end
-	
-	post "/register" do
-		if User.find_by(username: params[:username]) || User.find_by_slug(params[:username])
-			flash[:notice] = "the username \"#{params[:username]}\" is unavailable"
-			redirect "/register"
-		elsif !params.values.any? { |value| value.blank? }
-			user = User.create(params)
-			session[:user_id] = user.id
-			redirect "/users/#{user.slug}"
-		end
-		flash[:notice] = "username/password must not be blank"
-		redirect "/register"
-	end
-
-	get "/login" do
-		erb :login
-	end
-
-	post "/login" do
-		user = User.find_by(username: params[:username])
-		if user && user.authenticate(params[:password])
-			session[:user_id] = user.id
-			redirect "/users/#{user.slug}"
-		end
-		flash[:notice] = "invalid username and/or password"
-		redirect "/login"
-	end
-
-	get "/logout" do
-		session.clear
-		redirect '/'
-	end
-
 	helpers do
 		
 		def current_user
--- config.ru
+++ config.ru
@@ -1,6 +1,7 @@
 require "./config/environment"
 
 use Rack::MethodOverride
+use SessionsController
 use UserController
 use PostController
 run ApplicationController
